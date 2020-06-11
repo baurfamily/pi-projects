190b87317366bdc048751d8e2cc6298749857183
Eric Baur <baurfamily@gmail.com>
2020-06-11 16:15:59 -0700

    wall class started

--- minecraft/block_test.py
+++ minecraft/block_test.py
@@ -3,24 +3,40 @@ import mcpi.block as block
 from random import randint
 from time import sleep
 
+class Wall:
+    def __init__(self, x, y, z):
+        self.x, self.y, self.z = x, y, z
+        self.was_near_block = False
+        self.distance = 2
+            
+    def checkDistance(self, source, target, epsilon=0):
+        return source >= target - epsilon and source <= target + epsilon
+    
+    def isNear(self, x, z):
+        return self.checkDistance(self.x, x, self.distance) and self.checkDistance(self.z, z, self.distance)
+    
+    def show(self):
+        mc.setBlocks(self.x, self.y, self.z, self.x, self.y+1, self.z, block.STONE)
+    
+    def hide(self):
+        mc.setBlocks(self.x, self.y, self.z, self.x, self.y+1, self.z, block.AIR)
+
 mc = Minecraft.create()
 
-x, y, z = 0, 8, 12
+w = Wall(0, 8, 12)
 
-was_near_block = False
-
-def checkDistance(source, target, epsilon=0):
-    return source >= target - epsilon and source <= target + epsilon
 
 while True:
     ppos = mc.player.getTilePos()
-    if checkDistance(ppos.x, x, 1) and checkDistance(ppos.z, z, 1):
-        if not was_near_block:
+    if w.isNear(ppos.x, ppos.z):
+        if not w.was_near_block:
+            w.show()
             print('your on the block')
             mc.postToChat('your on the block')
-            was_near_block = True
+            w.was_near_block = True
     else:
-        if was_near_block:
+        if w.was_near_block:
+            w.hide()
             print('you moved away')
             mc.postToChat('you moved away')
-            was_near_block = False
+            w.was_near_block = False
